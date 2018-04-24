5ac4579ffe8f3774694aa03bee5671de4685f44b
戴煜炜 <kingdyw@126.com>
2018-04-24 23:41:49 +0800

    搭建前台页面布局

--- Presentation/CSCZJ.Web/src/app/app.module.ts
+++ Presentation/CSCZJ.Web/src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
+import { HttpModule } from '@angular/http'
+import { AppRoutingModule } from './/app-routing.module'
+
+import { ElModule } from 'element-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,    
+    ElModule.forRoot(),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
